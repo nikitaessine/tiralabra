e6246bb7b647ea2ac711925dbd0739407c553acc
Nikita Essine <nikita.essine@helsinki.fi>
2023-03-22 16:58:36 +0200

    labyrintin olio

---
+++ src/labyrintti.py
@@ -0,0 +1,11 @@
+class Labyrintti:
+    """Luokka, joka luo labyrinttiolion """
+    def __init__(self,leveys, korkeus):
+        """Luokan konstruktori
+
+        Args:
+            leveys: labyrintin leveys
+            korkeus: labyrintin korkeus
+        """
+        self.leveys = leveys
+        self.korkeus = korkeus
