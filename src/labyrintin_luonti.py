538affb9683da3ddd1efc0559097122da81e0774
Nikita Essine <nikita.essine@helsinki.fi>
2023-03-22 16:57:45 +0200

    luodaan ruudukko täynnä seiniä

---
+++ src/labyrintin_luonti.py
@@ -0,0 +1,17 @@
+from labyrintti import Labyrintti
+
+class Labyrintinluonti():
+
+    def __init__(self, labyrintti:Labyrintti):
+
+        self.korkeus = labyrintti.korkeus
+        self.leveys = labyrintti.leveys
+
+    def luo(self):
+
+        lista = [["#" for j in range(self.leveys)] for i in range(self.korkeus)]
+
+        for i in lista:
+            print(i)
+
+
