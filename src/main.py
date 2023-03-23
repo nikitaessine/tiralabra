ea0b332941a8d6d46e4119427cf0c13100963658
Nikita Essine <nikita.essine@helsinki.fi>
2023-03-23 09:54:45 +0200

    aloituspisteen kysely lisätty

--- src/main.py
+++ src/main.py
@@ -5,8 +5,10 @@ def main():
 
     korkeus = int(input("Anna korkeus: "))
     leveys = int(input("Anna leveys: "))
+    aloitus_x = int(input("Anna aloitus x-koordinaatti"))
+    aloitus_y = int(input("Anna aloitus y-koordinaatti"))
 
-    labyrintti_olio = Labyrintti(leveys, korkeus)
+    labyrintti_olio = Labyrintti(leveys, korkeus, aloitus_x, aloitus_y)
     Labyrintinluonti.luo(labyrintti_olio)
 
 if __name__ == "__main__":
