6ffbbb3bba18b0dda3e267923004fb8ea3e86f6a
Nikita Essine <nikita.essine@helsinki.fi>
2023-03-23 14:46:26 +0200

    CI kokeilu

--- src/labyrintin_luonti.py
+++ src/labyrintin_luonti.py
@@ -66,7 +66,7 @@ class Labyrintinluonti():
             y: ruudun y-koordinaatti
 
         Returns:
-            list: oikeista naapureista
+            list: oikeista naapurei
         """
 
         lista_naapureista = [(x, y-1), (x, y+1), (x+1, y), (x-1, y)]
