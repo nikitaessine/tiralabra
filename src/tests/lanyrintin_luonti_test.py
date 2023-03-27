3218cc783cf25249800c369ddc3f401089e9f1a9
Nikita Essine <nikita.essine@helsinki.fi>
2023-03-27 17:07:42 +0300

    lisää testejä

--- src/tests/lanyrintin_luonti_test.py
+++ src/tests/lanyrintin_luonti_test.py
@@ -4,7 +4,15 @@ from labyrintti import Labyrintti
 
 class TestLabyrintti(unittest.TestCase):
     def setUp(self):
-        self.labyrintti = Labyrintinluonti(Labyrintti(5, 5, 1, 1))
+        self.labyrintti = Labyrintinluonti(Labyrintti(5, 5, 0, 0))
 
     def test_luo_ruudukko(self):
         self.assertEqual(len(self.labyrintti.ruudukko), 5)
+    
+    def test_aloitusruutu_oikein(self):
+        self.labyrintti.luo(1, 1)
+        self.assertEqual(self.labyrintti.ruudukko[0][0], ".")
+
+    def test_palauta_labyrintti(self):
+        self.labyrintti.palauta()
+        self.assertEqual(self.labyrintti.ruudukko[1][1], ".")
