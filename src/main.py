d78c20777bf74881a3f18d807da7603c2b4b75e5
Nikita Essine <nikita.essine@helsinki.fi>
2023-03-30 08:30:47 +0300

    pääohjelman päivitys

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from labyrintti import Labyrintti
 from labyrintin_luonti import Labyrintinluonti
+from dead_end_filling import DeadEndFilling
+import datetime
 
 def main():
 
@@ -11,8 +13,16 @@ def main():
     labyrintti_olio = Labyrintti(leveys, korkeus, aloitus_x, aloitus_y)
     labyrintti = Labyrintinluonti(labyrintti_olio).palauta()
 
+    dead_end_filling_polku = DeadEndFilling(labyrintti)
+
     for i in labyrintti:
         print(i)
 
+    alku = datetime.datetime.now()
+    dead_end_filling_polku.dead_endit()
+    loppu = datetime.datetime.now()
+
+    print(f'Aikaa kului dead-end filling algoritmilla: {loppu-alku} sekunttia')
+    
 if __name__ == "__main__":
     main()
